c13d94915c04a52ca77c225b2b7da32f3ff919a3
Josh Marquardt <joshua.marquardt001@students.swic.edu>
2025-03-06 19:15:43 -0600

    Using Arrow Functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-var x = 3;
-
-console.log(x);
+const backpackFullOfAmmo = [
+    {item: 'energy cell', quantity: 10, priceEa: 0.2 },
+    {item: 'bullets', quantity: 5, priceEa: 0.5 },
+    {item: 'box of rockets', quantity: 5, priceEa: 25.0 },
+    {item: 'box of shells', quantity: 8, priceEa: 1.0 }
+    ]
+    function calcOrderCost(order) {order.quantity * order.priceEa
+    }
+    const totalValue = backpackFullOfAmmo.map((ammo) => {
+        console.log(ammo)
+    })
+    
+    console.log(totalValue)
